c94f981847f91ce3e25d1664e1e53cb9756271fd
Wen Jie <leewenjie.wjl@gmail.com>
2023-02-22 19:05:22 -0600

    Resolved HTTP Call issue by joining two threads before proceeding with rest of the code

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -27,8 +27,10 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.SocketTimeoutException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -40,64 +42,61 @@ import java.util.regex.Pattern;
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
-    String fetchData;
-    ProgressDialog pd;
-    GetMethodDemo getData;
+    String server_response;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getData = new GetMethodDemo();
-        getData.execute("http://fetch-hiring.s3.amazonaws.com/hiring.json");
-        HashMap<String, List<String>> JSONData = storeJSONData();
+
+        fetchJSONURL("https://fetch-hiring.s3.amazonaws.com/hiring.json");
+        HashMap<String, List<String>> JSONData = storeJSONData(server_response);
         List<String> displayList = sortJSONData(JSONData);
 
         listView = findViewById(R.id.list);
-        ArrayAdapter<String>  arr =
+        ArrayAdapter<String> arr =
                 new ArrayAdapter<String>(
-                this,
-                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-                displayList);
+                        this,
+                        androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                        displayList);
 
         listView.setAdapter(arr);
     }
 
 
-    public HashMap<String, List<String>> storeJSONData() {
+    public HashMap<String, List<String>> storeJSONData(String JSONData) {
         HashMap<String, List<String>> result = new HashMap<String, List<String>>();
-        String JSONData = getData.server_response;
-        if(JSONData == null) {
+        if (JSONData == null) {
             Log.d("storeJson", "null");
         }
         try {
             JSONArray arr = new JSONArray(JSONData);
-            for(int i=0; i<arr.length();i++){
-                    JSONObject element = arr.getJSONObject(i);
-                    if(!element.getString("name").equals("null") && !element.getString("name").trim().isEmpty()) {
-                        List<String> jsonNameList = result.get(element.getString("listId"));
-                        if(jsonNameList == null){
-                            //List does not exist for the particular key. Create it.
-                            jsonNameList = new ArrayList<String>();
-                            result.put(element.getString("listId"), jsonNameList);
-                        }
-                        if(!jsonNameList.contains(element.getString("name"))) {
-                            jsonNameList.add(element.getString("name"));
-                        }
-
+            for (int i = 0; i < arr.length(); i++) {
+                JSONObject element = arr.getJSONObject(i);
+                if (!element.getString("name").equals("null") && !element.getString("name").trim().isEmpty()) {
+                    List<String> jsonNameList = result.get(element.getString("listId"));
+                    if (jsonNameList == null) {
+                        //List does not exist for the particular key. Create it.
+                        jsonNameList = new ArrayList<String>();
+                        result.put(element.getString("listId"), jsonNameList);
                     }
+                    if (!jsonNameList.contains(element.getString("name"))) {
+                        jsonNameList.add(element.getString("name"));
+                    }
+
                 }
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
         return result;
     }
-    public List<String> sortJSONData(HashMap<String, List<String>> JSONData){
+
+    public List<String> sortJSONData(HashMap<String, List<String>> JSONData) {
         ArrayList<String> displayList = new ArrayList<>();
-        for(int i =1;i<=JSONData.size();i++){
-            if(JSONData.get(""+i)!=null) {
+        for (int i = 1; i <= JSONData.size(); i++) {
+            if (JSONData.get("" + i) != null) {
                 Collections.sort(JSONData.get("" + i), (s1, s2) -> {
 
                     String[] s1Parts = s1.split(" +");
@@ -132,7 +131,8 @@ public class MainActivity extends AppCompatActivity {
 
         return displayList;
     }
-    public String loadJSON(){
+
+    public String loadJSON() {
         String json = null;
         try {
             InputStream is = getAssets().open("hiring.json");
@@ -140,15 +140,61 @@ public class MainActivity extends AppCompatActivity {
             byte[] buffer = new byte[size];
             is.read(buffer);
             is.close();
-            json = new String(buffer, "UTF-8");
+            json = new String(buffer, StandardCharsets.UTF_8);
         } catch (IOException ex) {
             ex.printStackTrace();
             return null;
         }
         return json;
     }
+    private String readStream(InputStream in) {
+        BufferedReader reader = null;
+        StringBuffer response = new StringBuffer();
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return response.toString();
+    }
+    private void fetchJSONURL(String param){
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                URL url = null;
+                try {
+                    url = new URL(param);
+                    HttpURLConnection urlConnection = null;
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setRequestMethod("GET");
+                    server_response = readStream(urlConnection.getInputStream());
 
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        t.start();
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
 
 
 
