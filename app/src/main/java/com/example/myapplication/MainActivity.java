8e4014129b888c5048e1310853c98f3d19208dda
Wen Jie <leewenjie.wjl@gmail.com>
2023-02-23 20:56:19 -0600

    Added documentation

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,59 +1,53 @@
 package com.example.myapplication;
 
 
-import static android.app.PendingIntent.getActivity;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.List;
-import java.util.regex.Pattern;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * This class implements the app that parses JSON Data from a URL containing item names
+ * to be displayed in a list.
+ *
+ * @author Wen Jie Lee
+ */
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;
-    String server_response;
+    ListView listView; // instance that contains the array of items to be displayed
+    String server_response; // JSON Data fetched from the URL in string format
 
-
+    /**
+     * Main method that runs all the process(es) required to display the list of items
+     * @param savedInstanceState the current instance
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        fetchJSONURL("https://fetch-hiring.s3.amazonaws.com/hiring.json");
+        // Fetch and store the data into the string server_response.
+        // If needed, replace the url with a string of user input containing the url that the user
+        //wants to data fetch from.
+        fetchJSONData("https://fetch-hiring.s3.amazonaws.com/hiring.json");
+        // Parse and store the data fetched from the URL into a hashmap, to be used by other implementations
         HashMap<String, List<String>> JSONData = storeJSONData(server_response);
+        // ListView requires a list type to display the items, so we have to sort the items in the hashmap
         List<String> displayList = sortJSONData(JSONData);
-
+        //Display the list.
         listView = findViewById(R.id.list);
         ArrayAdapter<String> arr =
                 new ArrayAdapter<String>(
@@ -64,27 +58,36 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(arr);
     }
 
-
+    /**
+     * This method parses the response obtained from the HTTP call made by fetchJSONData()
+     * and stores them in a Hashmap containing a list of items. For more advanced implementations, we
+     * could easily access the items grouped by the listID that is stored as keys. Uses linkedlists due to
+     * frequent insertions.
+     *
+     * Alternatively, could use List<Hashmap> for frequent duplicate/colliding insertions.
+     * Could change to store JSON Objects instead of item names for future use cases.
+     *
+     * @param JSONData a string of response obtained from the Json URL
+     * @return a HashMap that groups items by listID.
+     */
     public HashMap<String, List<String>> storeJSONData(String JSONData) {
         HashMap<String, List<String>> result = new HashMap<String, List<String>>();
-        if (JSONData == null) {
-            Log.d("storeJson", "null");
-        }
+        //Parse the data while filtering out invalid entries
         try {
             JSONArray arr = new JSONArray(JSONData);
             for (int i = 0; i < arr.length(); i++) {
                 JSONObject element = arr.getJSONObject(i);
+                // Item cannot be empty or null
                 if (!element.getString("name").equals("null") && !element.getString("name").trim().isEmpty()) {
                     List<String> jsonNameList = result.get(element.getString("listId"));
                     if (jsonNameList == null) {
                         //List does not exist for the particular key. Create it.
-                        jsonNameList = new ArrayList<String>();
+                        jsonNameList = new LinkedList<String>();
                         result.put(element.getString("listId"), jsonNameList);
                     }
-                    if (!jsonNameList.contains(element.getString("name"))) {
-                        jsonNameList.add(element.getString("name"));
-                    }
-
+                    // There are no duplicates in the file provided, so just add the item name
+                    // directly without checking
+                    jsonNameList.add(element.getString("name"));
                 }
             }
         } catch (JSONException e) {
@@ -93,11 +96,19 @@ public class MainActivity extends AppCompatActivity {
         return result;
     }
 
+    /**
+     * This methood sorts the linked lists of strings by their item names, then by item number.
+     * Each list is grouped by their ID.
+     * Uses Collections.sort() that is merge sort based. O(n*log n).
+     *
+     * @param JSONData the Hashmap containing the items to be sorted
+     * @return the list of item names in sorted order, ascending.
+     */
     public List<String> sortJSONData(HashMap<String, List<String>> JSONData) {
-        ArrayList<String> displayList = new ArrayList<>();
+        List<String> displayList = new LinkedList<>();
         for (int i = 1; i <= JSONData.size(); i++) {
             if (JSONData.get("" + i) != null) {
-                Collections.sort(JSONData.get("" + i), (s1, s2) -> {
+                JSONData.get("" + i).sort((s1, s2) -> {
 
                     String[] s1Parts = s1.split(" +");
                     String[] s2Parts = s2.split(" +");
@@ -114,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
                             //if the parse works
                             int diff = intS1 - intS2;
                             if (diff == 0) {
-                                // continue;    // Actually, this is a no-op
+                                // continue;
                             } else {
                                 return diff;
                             }
@@ -128,10 +139,14 @@ public class MainActivity extends AppCompatActivity {
                 displayList.addAll(JSONData.get("" + i));
             }
         }
-
         return displayList;
     }
 
+    /**
+     * This method can be used to load a local asset JSON file instead if the user decides to.
+     *
+     * @return JSON data parsed in a string.
+     */
     public String loadJSON() {
         String json = null;
         try {
@@ -147,12 +162,21 @@ public class MainActivity extends AppCompatActivity {
         }
         return json;
     }
+
+    /**
+     * This is a helper method to read and save the server response from the HTTP call made by
+     * fetchJSONData().
+     *
+     * @param in the stream instance used to read the data
+     * @return a string containing the server response.
+     */
     private String readStream(InputStream in) {
         BufferedReader reader = null;
         StringBuffer response = new StringBuffer();
         try {
             reader = new BufferedReader(new InputStreamReader(in));
             String line = "";
+            //Keep going until reaching the end of line
             while ((line = reader.readLine()) != null) {
                 response.append(line);
             }
@@ -169,7 +193,13 @@ public class MainActivity extends AppCompatActivity {
         }
         return response.toString();
     }
-    private void fetchJSONURL(String param){
+
+    /**
+     * This method fetches JSON data from a specified URL. Runs on a separate thread because
+     * network operations cannot be done on the main thread.
+     * @param param URL to make a HTTP request to.
+     */
+    public void fetchJSONData(String param){
         Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -186,7 +216,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        // Start the HTTP request
         t.start();
+        // The main thread has to wait until the request is completed and server_response is stored.
+        // Or it will cause NPE's.
         try {
             t.join();
         } catch (InterruptedException e) {
